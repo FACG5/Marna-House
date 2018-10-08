30fd1b2bff4580488cf665edfb3313a93b01494d
donia98 <nadooshah98@gmail.com>
2018-10-08 07:21:51 +0300

    admin login route & view relates #14

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const adminLogin = require('./admin_login');
 const home = require('./home');
 
 router.get('/', home.get);
 router.post('/available-rooms', home.availableRooms);
 router.get('/room-details/:id', home.roomDetails);
+router.get('/login', adminLogin.get);
 
 module.exports = router;
