9496a631ff44fb99050c9e9ff28fd28d3ddf9181
donia98 <nadooshah98@gmail.com>
2018-10-08 14:34:16 +0300

    add reservation route and controller #17

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,5 +1,6 @@
 const fakeData = require('./../model/fakeData');
 const { avaliableRooms, getRoom } = require('./../model/queries/rooms');
+const { addReservation } = require('./../model/queries/reservations');
 
 exports.get = (req, res) => {
   res.render('home', {
@@ -30,4 +31,15 @@ exports.roomDetails = (req, res) => {
   getRoom(req.params.id)
     .then(room => res.send({ result: room.rows }))
     .catch(() => res.send({ Error: 'there is error' }));
+};
+
+exports.addReservations = (req, res) => {
+  addReservation(req.body)
+
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      throw new Error('add reservation error');
+    });
 };
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,5 +4,6 @@ const home = require('./home');
 router.get('/', home.get);
 router.post('/available-rooms', home.availableRooms);
 router.get('/room-details/:id', home.roomDetails);
+router.post('/reservations', home.addReservations);
 
 module.exports = router;
