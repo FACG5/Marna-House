1b35467f4ff4a42919f8f8d37950115fe7eb094c
donia98 <nadooshah98@gmail.com>
2018-10-09 12:46:40 +0300

    add reservation relates #39

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,7 @@
 const fakeData = require('./../model/fakeData');
 const { avaliableRooms, getRoom } = require('./../model/queries/rooms');
 const { addReservation } = require('./../model/queries/reservations');
+const { addUser } = require('./../model/queries/users');
 
 exports.get = (req, res) => {
   res.render('home', {
@@ -34,12 +35,38 @@ exports.roomDetails = (req, res) => {
 };
 
 exports.addReservations = (req, res) => {
-  addReservation(req.body)
-
+  const {
+    firstName: first_name,
+    lastName: last_name,
+    phoneNum: phone_num,
+    emailAddress: email_address,
+  } = req.body.userInfo;
+  addUser({
+    first_name, last_name, phone_num, email_address,
+  })
     .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      throw new Error('add reservation error');
+      const { id: user_id } = result.rows[0];
+      const { rooms, from: reservation_from, to: reservation_to } = req.body;
+      let counter = 0;
+      const resArray = [];
+      rooms.forEach((room_id) => {
+        addReservation({
+          user_id, reservation_from, reservation_to, room_id,
+        })
+          .then((reservationRes) => {
+            resArray.push({ room_id, msg: 'room bocked sucssesfully' });
+            counter++;
+            if (counter === rooms.length) {
+              res.send(resArray);
+            }
+          })
+          .catch((err) => {
+            resArray.push({ room_id, error: 'this roome not available choose another one !' });
+            counter++;
+            if (counter === rooms.length) {
+              res.send(resArray);
+            }
+          });
+      });
     });
 };
