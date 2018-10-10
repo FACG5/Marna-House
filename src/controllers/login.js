58a55ea10f402531031bdc0d2ea7bf78fa082ff2
knanahassouna1 <knanahassouna2@gmail.com>
2018-10-10 14:08:02 +0300

    add login and sign up

---
+++ src/controllers/login.js
@@ -0,0 +1,55 @@
+const bcrypt = require('bcryptjs');
+const { getUser } = require('./../model/queries/users');
+const { sign } = require('jsonwebtoken');
+
+exports.checkUser = (req, res, next) => {
+  const userObject = req.body;
+  getUser(userObject.email)
+    .then((result) => {
+      if (result) {
+        checkPassword(result.password, userObject.password)
+          .then(() => {
+            makeCookie({
+              email: result.email,
+            })
+              .then((cookieValue) => {
+                console.log(cookieValue);
+                res.cookie('jwt', cookieValue);
+                res.send({ redirect: '/' });
+              })
+              .catch(() => next());
+          })
+          .catch(() => res.send({ error: 'Check Email Or Password' }));
+      } else res.send({ error: 'Check Email Or Password ' });
+    })
+    .catch(() => res.send({ redirect: '/login' }));
+};
+
+
+exports.get = (req, res) => {
+  res.render('login', {
+    layout: 'auth', stylefile: 'login', domfile: 'login', title: 'Sign In',
+  });
+};
+
+const checkPassword = (hash, password) => new Promise((resolve, reject) => {
+  bcrypt.compare(password, hash, (err, result) => {
+    if (err) {
+      reject(err);
+    } else if (result) {
+      resolve(result);
+    } else {
+      reject(result);
+    }
+  });
+});
+
+const makeCookie = object => new Promise((resolve, reject) => {
+  sign(object, process.env.SECRET, (err, result) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
